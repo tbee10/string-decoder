da95147dfad8c134d4e1bba391f8e8e7c83db09a
Travis Bee <99930434+tbee10@users.noreply.github.com>
2022-03-26 16:04:32 -0600

    Add files via upload

---
+++ stringDecoder.js
@@ -0,0 +1,14 @@
+const decoder = code => {
+    let secretStr = code.split("")
+    let result = ""
+
+    for(let i = 0; i < secretStr.length; i ++){
+        if(!isNaN(+secretStr[i])){
+            i += +secretStr[i]
+        } else{
+            result += secretStr[i]
+        }
+    }
+    return result
+}
+console.log(decoder("2jgR1ko2yis0e"))
